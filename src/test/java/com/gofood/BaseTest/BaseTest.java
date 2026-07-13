8b6dc30c7dd6f8e1043d7ebd17443fea2931afda
SaifAli <saifali@SaifAlis-MacBook-Pro.local>
2026-07-13 11:34:34 +0530

    Update in basetest

--- src/test/java/com/gofood/BaseTest/BaseTest.java
+++ src/test/java/com/gofood/BaseTest/BaseTest.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
@@ -16,22 +15,21 @@ import com.gofood.Utility.ConfigReader;
 public class BaseTest {
 	protected SoftAssert softAssert;
 	private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
-
 	@Parameters("browser")
 	@BeforeClass
-	public void setup(@Optional("chrome")String browser) throws Exception {
+	public  void setup() throws Exception {
 		log.info("===Starting test on thread: {}====", Thread.currentThread().getId());
-		DriverFactory.initializeDriver(browser);
+		DriverFactory.initializeDriver(ConfigReader.getInstance().getConfig("browser"));
 		DriverFactory.getDriver().get(ConfigReader.getInstance().getConfig("url"));
 		DriverFactory.getDriver().manage().window().maximize();
 		new LoginPage().clickAcceptCookies();
 	}
-
 	@BeforeMethod
-	public void initSoftAssert() {
+	public void initSoftAssert()
+	{
 		softAssert = new SoftAssert();
 	}
-//
+
 	@AfterClass
 	public void teardown() {
 		log.info("=== Test finished, quitting driver ===");
